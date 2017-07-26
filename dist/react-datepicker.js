c2d4c1b756104fcc09b64ff6ffcc92b8eac0bec7
Andreas Idehaag <andreas.idehaag@cellip.se>
2017-07-26 15:40:53 +0200

    New build

--- dist/react-datepicker.js
+++ dist/react-datepicker.js
@@ -491,7 +491,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        var a = [];
 	        a.push(this.renderDateInput());
 	        if (this.props.isClearable && this.props.selected != null) a.push(this.renderClearButton());
-	        return a;
+	        return this.renderDateInput();
 	      }
 
 	      return _react2.default.createElement(_popper_component2.default, {
